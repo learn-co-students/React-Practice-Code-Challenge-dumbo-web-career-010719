42dc19a3c78a5cb170ba67eae3a214490562aeeb
github username <github email address>
2019-03-19 08:58:50 -0400

    done except load next 4

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,44 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [],
+    money: 100,
+    eaten: []
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushis => this.setState({ sushis }))
+  }
+
+  handleClickSushi = (e, props) => {
+    if(e.target.nodeName === 'IMG'){
+      const newArr = [...this.state.sushis]
+      let sush = newArr.find(sushi => sushi.img_url === e.target.src)
+      let cost = sush.price
+      let dupMoney = this.state.money
+      if(dupMoney >= cost) {
+        e.target.remove()
+        let newMoney = dupMoney - cost
+        console.log(newMoney);
+        this.setState({
+          money: newMoney,
+          eaten: [props, ...this.state.eaten]
+        })
+      } else {
+        alert('Sorry, go to the bank!')
+      }
+    }
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer sushis={this.state.sushis} handleClickSushi={this.handleClickSushi} />
+        <Table sushis={this.state.sushis} money={this.state.money} eaten={this.state.eaten} />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,15 +1,13 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+  let sushiPlates = props.sushis.map(sushiObj => <Sushi key={sushiObj.id} sushi={sushiObj} handleClickSushi={props.handleClickSushi} />)
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
+        {sushiPlates.slice(0, 4)}
         <MoreButton />
       </div>
     </Fragment>
